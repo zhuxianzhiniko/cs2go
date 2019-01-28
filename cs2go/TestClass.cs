bf674ac34f28ba9dcfd116f7ad3b1b54a3011368
zhuxianzhi <zhuxianzhiniko@gmail.com>
2019-01-28 19:27:31 +0800

    修改测试用例

--- cs2go/TestClass.cs
+++ cs2go/TestClass.cs
@@ -3,37 +3,40 @@ using System.Collections.Generic;
 
 public class TestClass
 {
+    //Enum测试
     public enum MyTestEnum
     {
         One1,
         Tow1
     }
 
+    //数组字典测试
     public Dictionary<int, string> dic;
-
     public List<int> lists;
 
 
-    public bool Flg;
+    //静态/常量/只读常量测试
     public const int Max = 100;
     public static string Name = "MyName";
     public readonly bool OnlyFlg = false;
+
+
     public int Attack;
     public int Defense;
-    
-    
+    public bool Flg;
 
 
-  public static TestClass Init()
-   {
-       var testClass = new TestClass();
-       testClass.Attack = 10;
-       testClass.Defense = 20;
-       testClass.Flg = false;
-       return testClass;
-   }
- 
+    //实例化返回测试    
+    public static TestClass Init()
+    {
+        var testClass = new TestClass();
+        testClass.Attack = 10;
+        testClass.Defense = 20;
+        testClass.Flg = false;
+        return testClass;
+    }
 
+    //主要是函数过程测试
     public int GetHurt(List<string> list, TestClass testClass, int callNum)
     {
         //变量声明语法
@@ -47,52 +50,56 @@ public class TestClass
         float testFloat = 0f;
         double testdouble = 0f;
         string str = "sss";
-        int[] d = new int[] {1, 2};
-        List<int> d1 = new List<int> {1, 2, 3, 4};
-        
+        int[] TestArray = new int[] {1, 2};
+        List<int> TestList = new List<int> {1, 2, 3, 4};
+
         //API 转译
-        var listLen = d1.Count;
-        var arrLen = d.Length;
-        d1.RemoveAt(0);
-        d1.Add(100);
-        d1.Clear();
-        
+        var listLen = TestList.Count;
+        var arrLen = TestArray.Length;
+        TestList.RemoveAt(0);
+        TestList.Add(100);
+        TestList.Clear();
+
         // 字典，数组访问 局部变量，类函数，静态函数调用测试
         dic[100] = "dd";
         var b = list[0];
         dic[100] = list[0];
         StShow();
         ShowTest(100);
-        var attack =GetMaxAttack(100);
+        var attack = GetMaxAttack(100);
         GetMaxAttack(100);
         testClass.Show();
-        
+
         //实例化类测试
         var test = new TestClass();
+
+        //获得实例化测试
         var test1 = Init();
         test1.Show();
 
         //基础运算符
         var num1 = 100 + 2 - 10 * 100 / 2;
-        
+
         //for表达式
         for (int i = 10; i < testClass.lists.Count; i++)
         {
             i++;
         }
-        
+
         //switch表达式
         switch (testClass.GetMaxAttack(100))
         {
             case 1:
-                //嵌套测试
+                //嵌套测试 1
                 for (int i = 10; i < 100; i++)
                 {
                     i++;
+                    //嵌套测试 2
                     for (int k = 10; k < 100; k++)
                     {
                         k++;
 
+                        //嵌套测试 3
                         switch (1)
                         {
                             case 1:
@@ -102,49 +109,49 @@ public class TestClass
                         }
                     }
                 }
+
                 break;
- 
-            
+
+
             //+= /= -= 测试
             case 2:
                 var k1 = 1;
-                k1+= 1;
+                k1 += 1;
                 break;
             case 3:
                 var k2 = 1;
-                k2-= 1;
+                k2 -= 1;
                 break;
             default:
                 var k3 = 1;
-                k3/= 1;
+                k3 /= 1;
                 break;
         }
-        
+
         //返回值
         return testClass.lists.Count;
     }
 
+    //类函数测试 1
     public void Show()
     {
     }
-    public int TestFunc(int num, string name)
+
+
+    //类函数测试 2
+    public int GetMaxAttack(int num)
     {
         return 100;
     }
 
-
+    //静态函数测试 1
     public static int ShowTest(int num)
     {
         return 100;
     }
 
-
+    //静态函数测试 2
     public static void StShow()
     {
-    }
-
-    public int GetMaxAttack(int num)
-    {
-        return 100;
     }
 }
